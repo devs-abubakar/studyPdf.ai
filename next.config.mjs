2793acd6cdbb9ac0caaf51be8470823a5605d2f7
devs-abubakar <m.abubakar.devs@gmail.com>
2026-05-16 17:05:23 +0500

    fix: fixed some frontend bugs and added a demo gif

--- next.config.mjs
+++ next.config.mjs
@@ -1,11 +1,10 @@
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  experimental: {
-    turbopack: {
-      root: '.',
-    },
+  turbopack: {
+    root: '.',
   },
 };
+
 
 export default nextConfig;
